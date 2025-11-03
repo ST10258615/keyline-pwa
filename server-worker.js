4bacad78e3eee8f69459a949903b1949a3c6d7fc
Ethan Pillay <ST10258615@vcconnect.edu.za>
2025-11-03 11:45:21 +0200

    Initial PWA Setup

---
+++ server-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'pwa-simple-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
